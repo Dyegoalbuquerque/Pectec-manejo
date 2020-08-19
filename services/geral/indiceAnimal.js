36aac52330d8820cf3be44374f2cdf177405fbba
Dyego Vasconcelos de Albuquerque <dyegovasconcelosdealbuquerque@MacBook-Air-de-Dyego-2.local>
2020-08-18 21:01:54 -0300

    update

--- services/geral/indiceAnimal.js
+++ services/geral/indiceAnimal.js
@@ -2,22 +2,22 @@
 export class IndiceAnimal {
 
     static obterQuantidadeMarra(animais) {
-        return animais.filter(f => f.situacao == 'M').length;;
+        return animais.filter(f => f.tag == 'M').length;;
     }
 
     static obterQuantidadeGestacao(animais) {
-        return animais.filter(f => f.situacao == 'G').length;;
+        return animais.filter(f => f.tag == 'G').length;;
     }
 
     static obterQuantidadeLactacao(animais) {
-        return animais.filter(f => f.situacao == 'L').length;;
+        return animais.filter(f => f.tag == 'L').length;;
     }
 
     static obterQuantidadeConfirmacaoGestacao(animais) {
-        return animais.filter(f => f.situacao == 'CG').length;;
+        return animais.filter(f => f.tag == 'CG').length;;
     }
 
     static obterQuantidadeIDC(animais) {
-        return animais.filter(f => f.situacao == 'IDC').length;;
+        return animais.filter(f => f.tag == 'IDC').length;;
     }
 }
